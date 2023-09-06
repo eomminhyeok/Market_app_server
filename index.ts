c9728e5e8d382aba5b29ae938183242384207d6e
eomminhyeok <33335768+eomminhyeok@users.noreply.github.com>
2023-09-06 17:41:45 +0900

    commit22

---
+++ index.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import mysql from 'mysql';
+import bodyParser from 'body-parser';
+import usersRouter from './router/userinfo';
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = mysql.createConnection({
+  host: '127.0.0.1' ,
+  user: 'root',
+  password: 'Jela7839!',
+  database: 'market_app',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('DB 연결 실패:', err);
+    return;
+  }
+  console.log('DB 연결 성공.');
+});
+
+app.get('/', (req, res) => {
+  res.send('서버 접속 성공');
+});
+
+app.get('/user', (req, res) => {
+  const query = 'SELECT * FROM user';
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('유저 정보를 가져오기 실패:', err);
+      res.status(500).json({ error: '유저 정보 가져오기 실패'});
+      return;
+    }
+    res.json(results);
+  });
+});
+
+
+app.use('/userinfo', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export default db;
+
